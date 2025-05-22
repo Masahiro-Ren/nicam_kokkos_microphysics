d34c506767baaa5548f4b735e48d2b14b96e4584
Ren <renxzb@gmail.com>
2025-05-22 17:50:01 +0900

    transfering original GNU make to cmake

---
+++ config/archi_flags.cmake
@@ -0,0 +1,17 @@
+if(ARCHITECTURE STREQUAL "A64FX")
+#    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Nclang -Ofast -mcpu=a64fx -fvectorize \ 
+#                        -ffj-zfill=100 -ffj-prefetch-sequential=soft \
+#                        -ffj-prefetch-line=8 -ffj-prefetch-line-L2=16 \
+#                        -ffj-lst=t \
+#                        -DUSEOPENMP")
+    target_compile_options(phy.exe PRIVATE
+                            -Nclang -Ofast -mcpu=a64fx -fvectorize
+                            -ffj-zfill=100 -ffj-prefetch-sequential=soft 
+                            -ffj-prefetch-line=8 -ffj-prefetch-line-L2=16 
+                            -ffj-lst=t -DUSEOPENMP
+                            )
+elseif(ARCHITECTURE STREQUAL "INTEL")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -ftree-vectorize \ 
+                        -march=cascadelake \
+                        -DUSEOPENMP")
+endif()
