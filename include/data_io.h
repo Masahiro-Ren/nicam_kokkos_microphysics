9f5056def3794077ad52f1baa9cc8a5bc20a12fa
Ren <renxzb@gmail.com>
2025-07-09 15:36:01 +0900

    make read_data_nd capable for both host and device view

--- include/data_io.h
+++ include/data_io.h
@@ -112,39 +112,22 @@ constexpr size_t SIZE_BUF_4D = ADM_lall * TRC_VMAX * ADM_kall * ADM_gall_in * si
 constexpr size_t SIZE_BUF_4D2 = 2 * ADM_lall * ADM_KNONE * ADM_gall_in * sizeof(double);
 
 void read_data_1d(const std::string& filename, double arr1d[ADM_kall]);
+
+void read_data_2d(const std::string& filename, double arr2d[ADM_lall][ADM_gall_in]);
+
+void read_data_3d(const std::string& filename, double arr3d[ADM_lall][ADM_kall][ADM_gall_in]);
+void read_data_3d(const std::string& filename, double arr3d[ADM_lall][ADM_KNONE][ADM_gall_in]);
+
+void read_data_4d(const std::string& filename, double arr4d[ADM_lall][TRC_VMAX][ADM_kall][ADM_gall_in]);
+void read_data_4d(const std::string& filename, double arr4d[2][ADM_lall][ADM_KNONE][ADM_gall_in]);
+
 /**
  * Kokkos ver.
  */
 // void read_data_1d(const std::string& filename, View<double*>& arr1d);
-void read_data_1d(const std::string& filename, View1D<double, DEFAULT_MEM>::HostMirror arr1d);
+void read_data_1d(const std::string& filename, View1D<double, DEFAULT_MEM> arr1d);
+void read_data_2d(const std::string& filename, View2D<double, DEFAULT_MEM> arr2d);
+void read_data_3d(const std::string& filename, View3D<double, DEFAULT_MEM> arr3d);
+void read_data_4d(const std::string& filename, View4D<double, DEFAULT_MEM> arr4d);
 
-void read_data_2d(const std::string& filename, double arr2d[ADM_lall][ADM_gall_in]);
-/**
- * Kokkos ver.
- */
-// void read_data_2d(const std::string& filename, View<double**>& arr2d);
-void read_data_2d(const std::string& filename, View2D<double, DEFAULT_MEM>::HostMirror arr2d);
-
-void read_data_3d(const std::string& filename, double arr3d[ADM_lall][ADM_kall][ADM_gall_in]);
-void read_data_3d(const std::string& filename, double arr3d[ADM_lall][ADM_KNONE][ADM_gall_in]);
-/**
- * Kokkos ver.
- */
-// void read_data_3d(const std::string& filename, View<double***>& arr3d);
-void read_data_3d(const std::string& filename, View3D<double, DEFAULT_MEM>::HostMirror arr3d);
-
-void read_data_4d(const std::string& filename, double arr4d[ADM_lall][TRC_VMAX][ADM_kall][ADM_gall_in]);
-void read_data_4d(const std::string& filename, double arr4d[2][ADM_lall][ADM_KNONE][ADM_gall_in]);
-/**
- * Kokkos ver.
- */
-// void read_data_4d(const std::string& filename, View<double****>& arr4d);
-void read_data_4d(const std::string& filename, View4D<double, DEFAULT_MEM>::HostMirror arr4d);
-
-
-// template<size_t LDIM, size_t KDIM, size_t IJDIM>
-// void read_data_3d(const std::string& filename, double arr3d[LDIM][KDIM][IJDIM]);
-
-// template<size_t WDIM, size_t LDIM, size_t KDIM, size_t IJDIM>
-// void read_data_4d(const std::string& filename, double arr4d[WDIM][LDIM][KDIM][IJDIM]);
 }
--- src/data_io.cpp
+++ src/data_io.cpp
@@ -35,37 +35,6 @@ void read_data_1d(const std::string& filename, double arr1d[ADM_kall])
     }
 }
 
-/**
- * Kokkos ver.
- */
-// void read_data_1d(const std::string& filename, View<double*>& arr1d)
-void read_data_1d(const std::string& filename, View1D<double, DEFAULT_MEM>::HostMirror arr1d)
-{
-    std::ifstream infile(filename, std::ios::binary);
-
-    if(!infile)
-    {
-        std::cerr << __PRETTY_FUNCTION__ << " Error opening " << filename << std::endl;
-        DEBUG::ADM_Proc_stop();
-    }
-
-    if(!infile.read(BUF_1D, SIZE_BUF_1D))
-    {
-        std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
-        DEBUG::ADM_Proc_stop();
-    }
-
-    double* cast_array = reinterpret_cast<double*>(BUF_1D);
-
-    size_t dim0 = arr1d.extent_int(0);
-
-    auto range_1d = RangePolicy<Schedule<Kokkos::Static>, HOST_SPACE>(0, dim0);
-    Kokkos::parallel_for("read_data_1d", range_1d, 
-    KOKKOS_LAMBDA(const size_t i){
-        arr1d(i) = cast_array[i];
-    });
-}
-
 void read_data_2d(const std::string& filename, double arr2d[ADM_lall][ADM_gall_in])
 {
     std::ifstream infile(filename, std::ios::binary);
@@ -92,39 +61,6 @@ void read_data_2d(const std::string& filename, double arr2d[ADM_lall][ADM_gall_i
             arr2d[j][i] = cast_array[j * ADM_gall_in + i];
         }
     }
-}
-
-/**
- * Kokkos ver.
- */
-// void read_data_2d(const std::string& filename, View<double**>& arr2d)
-void read_data_2d(const std::string& filename, View2D<double, DEFAULT_MEM>::HostMirror arr2d)
-{
-    std::ifstream infile(filename, std::ios::binary);
-
-    if(!infile)
-    {
-        std::cerr << __PRETTY_FUNCTION__ << " Error opening " << filename << std::endl;
-        DEBUG::ADM_Proc_stop();
-    }
-
-    if(!infile.read(BUF_2D, SIZE_BUF_2D))
-    {
-        std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
-        DEBUG::ADM_Proc_stop();
-    }
-
-    double* cast_array = reinterpret_cast<double*>(BUF_2D);
-
-    size_t dim0 = arr2d.extent_int(0);
-    size_t dim1 = arr2d.extent_int(1);
-
-    auto range_2d = MDRangePolicy<HOST_SPACE, Kokkos::Rank<2>>({0,0},{dim0, dim1});
-    Kokkos::parallel_for("read_data_2d", range_2d, 
-    KOKKOS_LAMBDA(const size_t j, const size_t i){
-        arr2d(j, i) = cast_array[j * ADM_gall_in + i];
-    });
-    
 }
 
 void read_data_3d(const std::string& filename, double arr3d[ADM_lall][ADM_kall][ADM_gall_in])
@@ -189,63 +125,6 @@ void read_data_3d(const std::string& filename, double arr3d[ADM_lall][ADM_KNONE]
             }
         }
     }
-}
-
-/**
- * Kokkos ver.
- */
-// void read_data_3d(const std::string& filename, View<double***>& arr3d)
-void read_data_3d(const std::string& filename, View3D<double, DEFAULT_MEM>::HostMirror arr3d)
-{
-    std::ifstream infile(filename, std::ios::binary);
-
-    if(!infile)
-    {
-        std::cerr << __PRETTY_FUNCTION__ << " Error opening " << filename << std::endl;
-        DEBUG::ADM_Proc_stop();
-    }
-
-    size_t dim0 = arr3d.extent_int(0);
-    size_t dim1 = arr3d.extent_int(1);
-    size_t dim2 = arr3d.extent_int(2);
-
-    size_t IJ = dim1 * dim2;
-
-    double* cast_array = nullptr;
-
-    /* May have better implementation*/
-    if(dim1 == ADM_kall)
-    {
-        if(!infile.read(BUF_3D, SIZE_BUF_3D))
-        {
-            std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
-            DEBUG::ADM_Proc_stop();
-        }
-
-        cast_array = reinterpret_cast<double*>(BUF_3D);
-    }
-    else if(dim1 == ADM_KNONE)
-    {
-        if(!infile.read(BUF_3D2, SIZE_BUF_3D2))
-        {
-            std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
-            DEBUG::ADM_Proc_stop();
-        }
-
-        cast_array = reinterpret_cast<double*>(BUF_3D2);
-    }
-
-    if(cast_array == nullptr)
-    {
-        std::cerr << __PRETTY_FUNCTION__ << " cast_array is nullptr while reading " << filename << std::endl;
-        DEBUG::ADM_Proc_stop();
-    }
-
-    auto range_3d = MDRangePolicy<HOST_SPACE, Kokkos::Rank<3>>({0,0,0}, {dim0, dim1, dim2}); 
-    Kokkos::parallel_for("read_data_3d", range_3d, 
-    KOKKOS_LAMBDA(const size_t k, const size_t j, const size_t i){
-        arr3d(k, j, i) = cast_array[k * IJ + j * ADM_gall_in + i];
-    });
 }
 
 void read_data_4d(const std::string& filename, double arr4d[ADM_lall][TRC_VMAX][ADM_kall][ADM_gall_in])
@@ -320,6 +199,142 @@ void read_data_4d(const std::string& filename, double arr4d[2][ADM_lall][ADM_KNO
     }
 }
 
+
+/**
+ * ================================= Kokkos ver. ==============================================
+ */
+
+void read_data_1d(const std::string& filename, View1D<double, DEFAULT_MEM> arr1d)
+{
+    std::ifstream infile(filename, std::ios::binary);
+
+    if(!infile)
+    {
+        std::cerr << __PRETTY_FUNCTION__ << " Error opening " << filename << std::endl;
+        DEBUG::ADM_Proc_stop();
+    }
+
+    if(!infile.read(BUF_1D, SIZE_BUF_1D))
+    {
+        std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
+        DEBUG::ADM_Proc_stop();
+    }
+
+    double* cast_array = reinterpret_cast<double*>(BUF_1D);
+
+    size_t dim0 = arr1d.extent_int(0);
+
+    auto h_arr1d = Kokkos::create_mirror_view(arr1d);
+
+    auto range_1d = RangePolicy<Schedule<Kokkos::Static>, HOST_SPACE>(0, dim0);
+    Kokkos::parallel_for("read_data_1d", range_1d, 
+    KOKKOS_LAMBDA(const size_t i){
+        h_arr1d(i) = cast_array[i];
+    });
+
+    Kokkos::deep_copy(arr1d, h_arr1d);
+}
+
+
+/**
+ * Kokkos ver.
+ */
+// void read_data_2d(const std::string& filename, View<double**>& arr2d)
+void read_data_2d(const std::string& filename, View2D<double, DEFAULT_MEM> arr2d)
+{
+    std::ifstream infile(filename, std::ios::binary);
+
+    if(!infile)
+    {
+        std::cerr << __PRETTY_FUNCTION__ << " Error opening " << filename << std::endl;
+        DEBUG::ADM_Proc_stop();
+    }
+
+    if(!infile.read(BUF_2D, SIZE_BUF_2D))
+    {
+        std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
+        DEBUG::ADM_Proc_stop();
+    }
+
+    double* cast_array = reinterpret_cast<double*>(BUF_2D);
+
+    size_t dim0 = arr2d.extent_int(0);
+    size_t dim1 = arr2d.extent_int(1);
+
+    auto h_arr2d = Kokkos::create_mirror_view(arr2d);
+
+    auto range_2d = MDRangePolicy<HOST_SPACE, Kokkos::Rank<2>>({0,0},{dim0, dim1});
+    Kokkos::parallel_for("read_data_2d", range_2d, 
+    KOKKOS_LAMBDA(const size_t j, const size_t i){
+        h_arr2d(j, i) = cast_array[j * ADM_gall_in + i];
+    });
+    
+    Kokkos::deep_copy(arr2d, h_arr2d);
+}
+
+
+/**
+ * Kokkos ver.
+ */
+// void read_data_3d(const std::string& filename, View<double***>& arr3d)
+void read_data_3d(const std::string& filename, View3D<double, DEFAULT_MEM>::HostMirror arr3d)
+{
+    std::ifstream infile(filename, std::ios::binary);
+
+    if(!infile)
+    {
+        std::cerr << __PRETTY_FUNCTION__ << " Error opening " << filename << std::endl;
+        DEBUG::ADM_Proc_stop();
+    }
+
+    size_t dim0 = arr3d.extent_int(0);
+    size_t dim1 = arr3d.extent_int(1);
+    size_t dim2 = arr3d.extent_int(2);
+
+    size_t IJ = dim1 * dim2;
+
+    double* cast_array = nullptr;
+
+    /* May have better implementation*/
+    if(dim1 == ADM_kall)
+    {
+        if(!infile.read(BUF_3D, SIZE_BUF_3D))
+        {
+            std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
+            DEBUG::ADM_Proc_stop();
+        }
+
+        cast_array = reinterpret_cast<double*>(BUF_3D);
+    }
+    else if(dim1 == ADM_KNONE)
+    {
+        if(!infile.read(BUF_3D2, SIZE_BUF_3D2))
+        {
+            std::cerr << __PRETTY_FUNCTION__ << " Error reading " << filename << std::endl;
+            DEBUG::ADM_Proc_stop();
+        }
+
+        cast_array = reinterpret_cast<double*>(BUF_3D2);
+    }
+
+    if(cast_array == nullptr)
+    {
+        std::cerr << __PRETTY_FUNCTION__ << " cast_array is nullptr while reading " << filename << std::endl;
+        DEBUG::ADM_Proc_stop();
+    }
+
+    auto h_arr3d = Kokkos::create_mirror_view(arr3d);
+
+    auto range_3d = MDRangePolicy<HOST_SPACE, Kokkos::Rank<3>>({0,0,0}, {dim0, dim1, dim2}); 
+    Kokkos::parallel_for("read_data_3d", range_3d, 
+    KOKKOS_LAMBDA(const size_t k, const size_t j, const size_t i){
+        h_arr3d(k, j, i) = cast_array[k * IJ + j * ADM_gall_in + i];
+    });
+
+    Kokkos::deep_copy(arr3d, h_arr3d);
+}
+
+
 /**
  * Kokkos ver.
  */
@@ -371,11 +386,15 @@ void read_data_4d(const std::string& filename, View4D<double, DEFAULT_MEM>::Host
         DEBUG::ADM_Proc_stop();
     }
 
+    auto h_arr4d = Kokkos::create_mirror_view(arr4d);
+
     auto range_4d = MDRangePolicy<HOST_SPACE, Kokkos::Rank<4>>({0,0,0,0}, {dim0, dim1, dim2, dim3}); 
     Kokkos::parallel_for("read_data_4d", range_4d, 
     KOKKOS_LAMBDA(const size_t l, const size_t k, const size_t j, const size_t i){
-        arr4d(l, k, j, i) = cast_array[l * IJK + k * IJ + j * ADM_gall_in + i];
+        h_arr4d(l, k, j, i) = cast_array[l * IJK + k * IJ + j * ADM_gall_in + i];
     });
+
+    Kokkos::deep_copy(arr4d, h_arr4d);
 }
 
 }
